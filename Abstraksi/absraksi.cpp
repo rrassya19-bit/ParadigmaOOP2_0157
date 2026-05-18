1c17bfb976b139ed59764b79ae2601f4635e04d8
rrassya19-bit <rrassys19@gmail.com>
2026-05-18 14:45:00 +0700

    tambah: method setXY untuk mengisi nilai member private

--- Abstraksi/absraksi.cpp
+++ Abstraksi/absraksi.cpp
@@ -4,4 +4,15 @@ using namespace std;
 class AbstraksiKlas {
     private:
         string x, y;  // Hanya bisa diakses di dalam class (tersembunyi dari luar)
+
+    public:
+        // Method untuk mengisi nilai private member dari luar class
+        void setXY(string a, string b) {
+            x = a;  // Isi x dengan nilai parameter a
+            y = b;  // Isi y dengan nilai parameter b
+        }
 };
+
+int main() {
+    return 0;
+}
