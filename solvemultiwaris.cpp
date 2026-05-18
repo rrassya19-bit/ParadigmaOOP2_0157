41848240419ebd33669a453be95238e9c6f3d910
rrassya19-bit <rrassys19@gmail.com>
2026-05-18 15:18:13 +0700

    tambah: class pekerja dan pelajar inheritance biasa dari orang (tanpa virtual)

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -3,11 +3,26 @@ using namespace std;
 
 class orang {
     public:
-        int umur;  // Atribut umur yang bisa diakses class turunan
+        int umur;
 
-        // Constructor: isi umur dan cetak pesan saat objek dibuat
         orang(int pUmur) : umur(pUmur) {
             cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+        }
+};
+
+// Inheritance biasa (tanpa virtual) = orang akan dibuat 2x jika dipakai multiple inheritance
+class pekerja : public orang {
+    public:
+        pekerja(int pUmur) : orang(pUmur) {
+            cout << "pekerja dibuat" << umur << "\n" << endl;
+        }
+};
+
+// Inheritance biasa (tanpa virtual) = orang akan dibuat 2x jika dipakai multiple inheritance
+class pelajar : public orang {
+    public:
+        pelajar(int pUmur) : orang(pUmur) {
+            cout << "pelajar dibuat\n" << endl;
         }
 };
 
