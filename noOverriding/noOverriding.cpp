436520d562a449a2f65c4320932a52920a97be19
rrassya19-bit <rrassys19@gmail.com>
2026-05-18 15:00:22 +0700

    tambah: akses public pada class baseClass

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class baseClass {   // Deklarasi class induk bernama baseClass
+class baseClass {
+    public:   // Semua method di bawah bisa diakses dari luar class
 
 };
 
