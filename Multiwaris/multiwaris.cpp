ed31fd695dcb598a6feaa437b6cf813aa8439ea5
rrassya19-bit <rrassys19@gmail.com>
2026-05-18 15:07:41 +0700

    tambah: class orang sebagai base class dengan constructor dan atribut umur

--- Multiwaris/multiwaris.cpp
+++ Multiwaris/multiwaris.cpp
@@ -1,5 +1,15 @@
-#include <iostream>  
-using namespace std;  
+#include <iostream>
+using namespace std;
+
+class orang {
+    public:
+        int umur;  // Atribut umur yang bisa diakses class turunan
+
+        // Constructor: isi umur dan cetak pesan saat objek dibuat
+        orang(int pUmur) : umur(pUmur) {
+            cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+        }
+};
 
 int main() {
     return 0;
