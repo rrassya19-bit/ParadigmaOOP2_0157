0bed933309f6b78db7a93223d6163013a87a0038
rrassya19-bit <rrassys19@gmail.com>
2026-05-18 14:48:19 +0700

    tambah: class remoteLampu dengan array private saklarNo

---
+++ Enkapsulasi/enkapsulasi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+    private:
+        string saklarNo[10];  // Array private berisi 10 slot nama lampu
+};
+
+int main() {
+    return 0;
+}
