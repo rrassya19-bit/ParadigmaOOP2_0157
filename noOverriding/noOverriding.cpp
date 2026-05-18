d8d27dc325bf3a8a9598a1dab1d492560b433be5
rrassya19-bit <rrassys19@gmail.com>
2026-05-18 15:00:55 +0700

    tambah: deklarasi class derivedClass turunan dari baseClass

--- noOverriding/noOverriding.cpp
+++ noOverriding/noOverriding.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 class baseClass {
     public:
-        // virtual = bisa di-override oleh class turunan
-        // final   = TIDAK BOLEH di-override oleh class turunan (dikunci)
         virtual void perkenalan() final {
             cout << "hallo saya function dari base class";
         }
+};
+
+// derivedClass mewarisi semua public member dari baseClass
+class derivedClass : public baseClass {
+
 };
 
 int main() {
