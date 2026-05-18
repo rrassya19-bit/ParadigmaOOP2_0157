cfc83abcbcc676f156b91781a4fae48c7f038620
rrassya19-bit <rrassys19@gmail.com>
2026-05-18 14:54:18 +0700

    tambah: akses public pada class remoteLampu

--- Polimorpi/polimorpi.cpp
+++ Polimorpi/polimorpi.cpp
@@ -4,6 +4,9 @@ using namespace std;
 class remoteLampu {
     private:
         string saklarNo[10];  // Array private berisi 10 slot nama lampu
+
+    public:
+        // Method public akan ditambahkan di sini
 };
 
 int main() {
